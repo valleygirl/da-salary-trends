7bf8f8c346dd0f0ec736a92ff80a3c688fda9992
sindhu <sindhu.r050985@gmail.com>
2016-04-11 21:59:12 -0700

    Salary trends landing page + Search button

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/search', function(req, res){
+  res.sendFile(__dirname + '/search.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
